f768ba6ec818b9a4adf2c092ef5c4105f66746c2
Roland <34355155+rolandveen@users.noreply.github.com>
2024-05-27 20:23:52 +0200

    Trying to apply boruta

--- training.py
+++ training.py
@@ -7,6 +7,15 @@ but the resulting model model.joblib will be applied to the holdout data.
 It is important to document your training steps here, including seed, 
 number of folds, model, et cetera
 """
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+import joblib, random
+from submission import clean_df
+
+# loading data (predictors)
+train = pd.read_csv("..\\data\\training_data\\PreFer_train_data.csv", low_memory = False) 
+# loading the outcome
+outcome = pd.read_csv("..\\data\\training_data\\PreFer_train_outcome.csv") 
 
 def train_save_model(cleaned_df, outcome_df):
     """
@@ -34,3 +43,9 @@ def train_save_model(cleaned_df, outcome_df):
 
     # Save the model
     joblib.dump(model, "model.joblib")
+
+# preprocessing the data
+train_cleaned = clean_df(train)
+
+# training and saving the model
+train_save_model(train_cleaned, outcome)
